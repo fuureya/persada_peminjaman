d7cef4d3ab9fa436d698c2c884ec72269d5fac3b
fuureya <agiljibrin009@gmail.com>
2024-10-04 22:04:17 +0800

    custom pesan

--- backend/app/Http/Controllers/PeminjamanController.php
+++ backend/app/Http/Controllers/PeminjamanController.php
@@ -13,7 +13,10 @@ class PeminjamanController extends Controller
         $data = Peminjaman::paginate(10);
         if ($request->search) {
             $data = Peminjaman::where('nama_peminjam', 'like', '%' . $request->search . '%')->get();
-            return response()->json($data);
+            return response()->json([
+                'data' => $data,
+                'message' => 'data di temukan!!',
+            ]);
         }
         return response()->json($data);
     }
