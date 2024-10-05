97f951e3397915a97e82ea65a50138e720dec696
fuureya <agiljibrin009@gmail.com>
2024-10-05 23:57:05 +0800

    style: styling sedikit besok lanjut

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -8,6 +8,17 @@ import Fragment from '@/components/Fragment.vue';
             <div class="logo flex justify-center">
                 <img :src="'../../public/logo.png'" :alt="logo" class="w-32">
             </div>
+            <div class="layout-card">
+                <div class="grid grid-cols-12 border-red text-black">
+                    <div class="cols-span-6 ">
+                        test
+                    </div>
+                    <div class="cols-span-6 text-black">
+                        test
+                    </div>
+                </div>
+
+            </div>
         </div>
     </Fragment>
 </template>
