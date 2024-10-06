2ada6003498a8ee1350665569c7bf9b065cf943b
fuureya <agiljibrin009@gmail.com>
2024-10-06 10:05:59 +0800

    fix: bug label

--- frontend/src/components/InputForm.vue
+++ frontend/src/components/InputForm.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="mb-5">
-    <label for="{{ for }}" class="block mb-2 text-md font-medium font-weight-bold text-gray-900 dark:text-white">{{ title }}</label>
-    <input :type="type" id="{{ for }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" :placeholder=" placeholder " required />
-  </div>
+        <label :for="to" class="block mb-2 text-md font-medium font-weight-bold text-white dark:text-white">{{
+            title }}</label>
+        <input :type="type" :id="to"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-slate-950 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            :placeholder="placeholder" required />
+    </div>
 </template>
 
 <script setup>
@@ -12,7 +15,7 @@ const props = defineProps({
         type: String,
         default: 'text'
     },
-    for: String,
+    to: String,
     title: String,
     placeholder: String
 })
--- frontend/src/views/PeminjamanPage.vue
+++ frontend/src/views/PeminjamanPage.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+import Fragment from '@/components/Fragment.vue';
 import InputForm from '@/components/InputForm.vue';
 </script>
 
 <template>
-  <div class="bg-slate-950 text-white">
-    anjay
-  </div>
+  <Fragment>
+    <div class="container text-white">
+      <div class="logo flex justify-center">
+        <img :src="'/logo.png'" :alt="'logo'" class="w-32">
+      </div>
+      <div class="layout-card mt-16">
+        <InputForm to="nama_peminjam" title="Masukkan Nama Peminjam" placeholder="Nama" />
+        <InputForm to="kelas" title="Masukkan Kelas" placeholder="Kelas" />
+        <InputForm to="jurusan" title="Masukkan jurusan" placeholder="Jurusan" />
+      </div>
+    </div>
+  </Fragment>
 </template>
