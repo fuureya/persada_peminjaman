70ce2b112aa8f5fb7555309307553ed6b81992b4
fuureya <agiljibrin009@gmail.com>
2024-10-06 09:47:30 +0800

    style: fixing card peminjaman dan pengembaliat

--- frontend/src/components/Fragment.vue
+++ frontend/src/components/Fragment.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-[url('/bg.png')] w-full bg-contain bg-no-repeat p-28">
+    <div class="bg-[url('/bg.png')] w-full bg-cover bg-no-repeat p-10 md:p-28">
         <slot></slot>
     </div>
 </template>
--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -8,40 +8,36 @@ import Fragment from '@/components/Fragment.vue';
             <div class="logo flex justify-center">
                 <img :src="'/logo.png'" :alt="'logo'" class="w-32">
             </div>
-            <div class="layout-card mt-10">
-                <div class="grid grid-rows-3 border-red text-black">
-                    <div class="flex justify-between">
-                        <div class="w-1/2 pr-2">
-                            <RouterLink to="/peminjaman">
-                                <div class="bg-white shadow-md rounded-lg p-4">
-                                    <div class="flex justify-center">
-                                        <img :src="'pinjam.png'" :alt="'peminjaman'" class="w-50">
-                                    </div>
-                                    <p class="mt-4 pb-3 text-2xl font-bold text-center text-blue-950">
-                                        Peminjaman
-
-                                    </p>
+            <div class="layout-card mt-16">
+                <div class="grid grid-cols-2 gap-4 border-red text-black">
+                    <div class="flex justify-center">
+                        <RouterLink to="/peminjaman">
+                            <div class="bg-white shadow-md rounded-lg p-4 px-10">
+                                <div class="flex justify-center">
+                                    <img :src="'pinjam.png'" :alt="'peminjaman'" class="w-50">
                                 </div>
-                            </RouterLink>
-                        </div>
+                                <p class="mt-4 pb-3 text-2xl font-bold text-center text-blue-950">
+                                    Peminjaman
+                                </p>
+                            </div>
+                        </RouterLink>
+                    </div>
 
-                        <div class="w-1/2 pr-2">
-                            <RouterLink to="/pengembalian">
-                                <div class="bg-white shadow-md rounded-lg p-4">
-                                    <div class="flex justify-center">
-                                        <img :src="'kembali.png'" :alt="'pengembalian'" class="w-50">
-                                    </div>
-                                    <p class="mt-4 text-2xl font-bold text-center text-blue-950">
-                                        Pengembalian
-                                    </p>
+                    <div class="flex justify-center">
+                        <RouterLink to="/pengembalian">
+                            <div class="bg-white shadow-md rounded-lg p-4 px-10">
+                                <div class="flex justify-center">
+                                    <img :src="'kembali.png'" :alt="'pengembalian'" class="w-50">
                                 </div>
-                            </RouterLink>
-                        </div>
-
+                                <p class="mt-4 text-2xl font-bold text-center text-blue-950">
+                                    Pengembalian
+                                </p>
+                            </div>
+                        </RouterLink>
                     </div>
                 </div>
-
             </div>
         </div>
+
     </Fragment>
 </template>
