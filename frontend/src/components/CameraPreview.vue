4aeb2dc9c278503dc96cc1778d05fcdfe86391f2
fuureya <agiljibrin009@gmail.com>
2024-10-07 14:19:59 +0800

    fix: perbaiki kembali preview

--- frontend/src/components/CameraPreview.vue
+++ frontend/src/components/CameraPreview.vue
@@ -2,7 +2,7 @@
     <div v-if="image" class="flex justify-center">
         <img :src="image" :alt="'gambar preview'" id="preview">
     </div>
-    <div v-if="image == ''" class="flex justify-center">
+    <div class="flex justify-center">
         <WebCamUI :fullscreenState="false" @photoTaken="photoTaken" />
     </div>
 </template>
@@ -34,13 +34,7 @@ export default {
     },
     methods: {
         photoTaken(data) {
-
-            // console.log('image blob: ', data.blob)
-            console.log('image blob: ', data)
-            // console.log('image data url', data.image_data_url)
-
             this.image = data.image_data_url;
-
         },
     },
     setup() {
